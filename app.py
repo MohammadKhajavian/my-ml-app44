896094b205f7b351a35c7fa166222d42fbdfa88f
mohammad <mohamadkhajavian@yahoo.com>
2025-01-11 22:27:14 +0800

    Update app to use PORT environment variable

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,6 @@ def predict():
     return jsonify({'predicted_removal': prediction[0]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the PORT environment variable (default to 5000 if not set)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
